854050766ce5b3cff731e8ea959c104882d605a5
Vincent van Dijck <vincent@voorhoede.nl>
2020-07-08 21:46:56 +0200

    work in progress

--- next.config.js
+++ next.config.js
@@ -1,17 +1,39 @@
+const withPWA = require('next-pwa')
 const withLess = require('@zeit/next-less')
 const webpack = require('webpack')
 const dotenv = require('dotenv')
 const { parsed: localEnv } = dotenv.config()
 const plugin = new webpack.EnvironmentPlugin(localEnv)
 
-module.exports = withLess({
+const config = withLess({
+	// Taken from: https://github.com/developit/nextjs-preact-demo/
 	webpack(config) {
+		const splitChunks = config.optimization && config.optimization.splitChunks;
+
+		if (splitChunks) {
+			const cacheGroups = splitChunks.cacheGroups;
+			const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+
+			if (cacheGroups.framework) {
+				cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+					test: preactModules,
+				});
+				cacheGroups.commons.name = 'framework';
+			} else {
+				cacheGroups.preact = {
+					name: 'commons',
+					chunks: 'all',
+					test: preactModules,
+				};
+			}
+		}
+
 		config.plugins.push(plugin)
 		config.module.rules.push({
 			test: /\.graphql$/,
 			use: 'raw-loader'
-		}
-)
+		})
+
 		return config
 	},
 	poweredByHeader: false,
@@ -19,4 +41,9 @@ module.exports = withLess({
 	env: {
 		NEXT_API_KEY: process.env.NEXT_API_KEY,
 	},
+	pwa: {
+		dest: 'public',
+	},
 })
+
+module.exports = process.env.NODE_ENV === 'development' ? config : withPWA(config);
