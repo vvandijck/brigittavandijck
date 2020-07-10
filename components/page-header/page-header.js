2693c121b6068968d8ca8135bc76979e5285a90d
Vincent van Dijck <vincent@voorhoede.nl>
2020-07-10 14:25:43 +0200

    switch title and subtitle

--- components/page-header/page-header.js
+++ components/page-header/page-header.js
@@ -7,8 +7,8 @@ const PageHeader = ({ image = {}, subtitle = '', title = '' }) => (
 			<ResponsiveImage image={image} />
 		</div>
 		<div className="page-header__content container">
-			<h2 className="page-header__content-subtitle h3">{subtitle}</h2>
-			<h1 className="page-header__content-title h1">{title}</h1>
+			<h2 className="page-header__content-subtitle h3">{title}</h2>
+			<h1 className="page-header__content-title h1">{subtitle}</h1>
 		</div>
 	</section>
 )
