936eb23e2f9f214f0486c8b36835f51323292cb1
Vincent van Dijck <vincent@voorhoede.nl>
2020-07-26 20:37:26 +0200

    work in progress

--- components/app-header/app-header.js
+++ components/app-header/app-header.js
@@ -5,7 +5,7 @@ const AppHeader = () => (
 	<header className="app-header">
 		<div className="container">
 			<div>
-				<Link href="/">
+				<Link href="/" prefetch={false}>
 					<a>
 						<img src="https://www.datocms-assets.com/24034/1595598326-logo.svg" widht="300" height="40" alt="Logo" />
 					</a>
@@ -14,21 +14,21 @@ const AppHeader = () => (
 			<nav className="app-header__nav">
 				<ul className="app-header__list list-inline">
 					<li>
-						<Link href="/services">
+						<Link href="/services" prefetch={false}>
 							<a className="app-button app-button--tertiary" title="Werkwijze">
 								Services
 							</a>
 						</Link>
 					</li>
 					<li>
-						<Link href="/about">
+						<Link href="/about" prefetch={false}>
 							<a className="app-button app-button--tertiary" title="Over">
 								Over
 							</a>
 						</Link>
 					</li>
 					<li>
-						<Link href="/contact">
+						<Link href="/contact" prefetch={false}>
 							<a className="app-button app-button--secondary" title="Contact">
 								Contact
 							</a>
