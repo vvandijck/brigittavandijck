26159661ea4602f46ffddb1e539487116f042b25
Vincent van Dijck <vincent@voorhoede.nl>
2021-02-08 22:58:18 +0100

    remove unused code

--- pages/_error.js
+++ pages/_error.js
@@ -1,16 +1,11 @@
 import '../styles/index.less'
 
-// Core
-import Head from 'next/head'
-
 // Components
 import AppFooter from '../components/app-footer/app-footer'
-import AppHead from '../components/app-head/app-head'
 import AppHeader from '../components/app-header/app-header'
 
 const Error = () => (
 	<React.Fragment>
-		{/* <AppHead meta={home.meta} seo={home.seo} /> */}
 		<AppHeader />
 		<main className="error">
 			<h1>This page does not exist.</h1>
